fd32c2731991f6a3c1a43276e2c20b7d10b3a52a
Terje Topland <terjetop@uia.no>
2023-04-12 12:38:37 +0200

    Sending function from parent to child, and gets the data

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View, TextInput} from 'react-native';
+import {useEffect, useState} from "react";
+
+
+
+export const Child = ({dataFromGrandChildToParent}) => {
+    const [messageFromChild, setMessageFromChild] = useState(0.0)
+
+    return (
+      <View>
+        <TextInput
+            placeholder={`Please enter the value`}
+            onChangeText={(txt) => setMessageFromChild(txt)}
+        />
+        <Button onPress={() => dataFromGrandChildToParent(messageFromChild)}>Test me</Button>
+      </View>
+    )
+}
+export const Parent = () => {
+    const [dataFromGrandChild, setDataFromGrandChild] = useState(0.0)
+    const dataFromGrandChildToParent = (messageFromChild) => {
+        console.log(`The message from the child is: ${messageFromChild}`)
+        setDataFromGrandChild(messageFromChild)
+    }
+
+    return (
+      <Child dataFromGrandChildToParent={dataFromGrandChildToParent}/>
+    )
+}
+
 
 export default function App() {
+    const [dataFromChild, setDataFromChild] = useState(0.0)
+
+    const SendButtonAndGetData = () => {
+
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test send function from parent to child</Text>
+      <Parent/>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +56,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// Parent component
