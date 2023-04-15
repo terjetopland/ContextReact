4a95a5f7500cd5edd3051dca643ed9c5fd377a60
Bjarte Skaar <Bjarte.Skaar@fhi.no>
2023-04-15 22:23:27 +0200

    summerer

--- components/Child.js
+++ components/Child.js
@@ -1,12 +1,19 @@
-import {Text, View} from "react-native";
+import {Button, Text, View} from "react-native";
 import {GranChild} from "./GranChild";
 
-export const Child = () => {
+export const Child = ({inputList, addInputField, inputHandler}) => {
     return (
         <View>
             <Text>I am Child</Text>
-            <GranChild/>
-            <GranChild/>
+            {inputList.map((input) => (
+                <GranChild
+                    key={input.id}
+                    id={input.id}
+                    value={input.value}
+                    handleInputChange={inputHandler}
+                />
+            ))}
+            <Button title={'Add input field'} onPress={addInputField}/>
         </View>
     )
 }
--- context/MyContext.js
+++ context/MyContext.js
@@ -1,15 +1,49 @@
 import {createContext, useState} from "react";
 
-const MyContext = createContext(null);
+const MyContext = createContext({});
 
 export const MyContextProvider = ({children}) => {
     const [area, setArea] = useState(0)
     const [isCalculating, setIsCalculating] = useState(false)
 
+    const [inputList, setInputList] = useState([])
 
+    const handleInputChange = (id, value) => {
+        // hvis verdien er ikke et tall(NaN, not a number), returner
+        if (isNaN(value)) {
+            return;
+        }
+
+        // hvis verdien er et tall, oppdater inputList
+        const newInputList = inputList.map((input) =>
+            input.id === id ? { ...input, value: parseFloat(value) || 0 } : input
+        );
+        console.log(id, value, newInputList)
+
+        // setter inputList
+        setInputList(newInputList);
+
+        // summerer alle verdier i inputList
+        const sum = newInputList.reduce((accumulator, input) => accumulator + input.value, 0);
+        setArea(sum);
+    };
+    const addInputField = () => {
+        setInputList([...inputList, { id: inputList.length, value: ''}]);
+    };
+
+    const value = {
+        area,
+        setArea,
+        isCalculating,
+        setIsCalculating,
+        inputList,
+        setInputList,
+        handleInputChange,
+        addInputField,
+    }
 
     return (
-        <MyContext.Provider value={{area, setArea, isCalculating, setIsCalculating}}>
+        <MyContext.Provider value={value}>
             {children}
         </MyContext.Provider>
     );
